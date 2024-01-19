470ad34a6351eb511ec250c3af06f845ced53a18
sivaprabhakar <sivaprabhakaran.94@gmail.com>
2024-01-19 18:24:29 +0530

    removed base url

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,24 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-import Home from './components/Home';
-import Dashboard from './components/Dashboard';
+
+
 import Login from './components/Login';
 import Signup from './components/Signup';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
 
-// Set your desired base URL here
-const baseUrl = "https://passwordreset-front.netlify.app";
+
 
 
 const App = () => {
   return (
    
-    <Router basename={baseUrl}>
+    <Router >
       <Routes>
         
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+     
         <Route path="/signup" element={<Signup />} />
         <Route path="/Forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
